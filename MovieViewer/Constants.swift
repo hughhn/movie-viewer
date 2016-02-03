034a7ed5eb2d0acc6f1d32ddcc74ec628b6bea5d
Hugh Nguyen <hugh.hn@gmail.com>
2016-02-02 23:10:30 -0800

    add Podfile.lock

---
+++ MovieViewer/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  MovieViewer
+//
+//  Created by Hieu Nguyen on 2/2/16.
+//  Copyright © 2016 Hugo Nguyen. All rights reserved.
+//
+
+import Foundation
---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Hieu Nguyen on 2/2/16.
+//  Copyright © 2016 Hugo Nguyen. All rights reserved.
+//
+
+import Cocoa
+
+class MovieCell: UITableViewCell {
+
+}
